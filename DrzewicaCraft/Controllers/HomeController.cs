e382aac725f745678c66bddb9672e56c03a69786
Aquerr <nerdianin@gmail.com>
2017-04-24 22:30:55 +0200

    Commented out Message on about site

--- DrzewicaCraft/Controllers/HomeController.cs
+++ DrzewicaCraft/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DrzewicaCraft.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //ViewBag.Message = "Your application description page.";
 
             return View();
         }
